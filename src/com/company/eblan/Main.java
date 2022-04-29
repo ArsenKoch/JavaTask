96aa5603d6ad4c12eaea03cb5d9ad58efa25cf7d
ArsenKoch <arsenkocaran@icloud.com>
2022-04-29 19:43:12 +0300

    animal class test2

---
+++ src/com/company/eblan/Main.java
@@ -0,0 +1,26 @@
+package com.company.eblan;
+
+class Main {
+    public static void main(String[] args) {
+        Animal tiger = new Animal("тигр", "Артём");
+        tiger.age = 15;
+        tiger.weight = (float) 300.6;
+        tiger.isSwim = true;
+        tiger.isWalk = true;
+        tiger.isFly = false;
+        tiger.display();
+
+        Animal sparrow = new Animal("воробей", 5);
+        sparrow.weight = 2;
+        sparrow.isSwim = false;
+        sparrow.isWalk = true;
+        sparrow.isFly = true;
+        sparrow.display();
+
+        Animal duck = new Animal("Утка", "Утя", 3, 5.8f, true, true, true);
+        duck.holiday(5);
+        duck.rename("Утя-Утя");
+        duck.display();
+    }
+}
+
